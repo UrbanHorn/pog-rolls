eef4ca0541f3e0ef9b9c31130677192a60d6e10c
UrbanHorn <wladqwerty@gmail.com>
2026-07-12 00:50:43 +0300

    Обновил проект

--- start-server.cmd
+++ start-server.cmd
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
-"C:\Users\user\.cache\codex-runtimes\codex-primary-runtime\dependencies\node\bin\node.exe" server.js
+node server.js
 echo.
 echo Сервер остановлен. Нажмите любую клавишу, чтобы закрыть это окно.
 pause >nul
